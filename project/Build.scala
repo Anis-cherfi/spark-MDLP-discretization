cfaa62efb5b24c7f6a69346798103f3441c93673
barrybecker <bbe@esi-group.com>
2018-04-25 06:52:41 -0700

    MIN-7288, MIN-7360 Upgrade to using spark 2.3.0 (release version). Remove local maven resolver. Fox formatting.

--- project/Build.scala
+++ project/Build.scala
@@ -7,27 +7,28 @@ object ProjectBuild extends Build {
     id = "root",
     base = file("."),
     settings = Defaults.coreDefaultSettings ++ Seq(
-	name := "spark-MDLP-discretization",
-			version := "0.3-SPARK-2.3.1-SNAPSHOT",
-	organization := "org.apache.spark",
-	scalaVersion := "2.11.6",
-	spName := "apache/spark-MDLP-discretization",
-			sparkVersion := "2.3.1-SNAPSHOT",
-	sparkComponents += "mllib",
+      name := "spark-MDLP-discretization",
+      version := "0.3-SPARK-2.3.0-SNAPSHOT",
+      organization := "org.apache.spark",
+      scalaVersion := "2.11.6",
+      spName := "apache/spark-MDLP-discretization",
+      sparkVersion := "2.3.0",
+      sparkComponents += "mllib",
 			publishTo := Some("Artifactory Realm" at "http://esi-components.esi-group.com/artifactory/snapshot"),
-	credentials += Credentials(Path.userHome / ".m2" / ".credentials"),
-	publishMavenStyle := true,
-	licenses += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html"),
-			resolvers += Resolver.mavenLocal,
+      credentials += Credentials(Path.userHome / ".m2" / ".credentials"),
+      publishMavenStyle := true,
+      licenses += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html"),
+      //resolvers += Resolver.mavenLocal,
 
-	libraryDependencies ++= Seq(
-		"joda-time" % "joda-time" % "2.9.4",
-		// dependencies for unit tests
-		"org.scalatest" %% "scalatest" % "2.2.4" % "test",
-		"junit" % "junit" % "4.12" % "test",
-		"org.apache.commons" % "commons-lang3" % "3.4" % "test"
-		)
-	))
+      libraryDependencies ++= Seq(
+        "joda-time" % "joda-time" % "2.9.4",
+        // dependencies for unit tests
+        "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+        "junit" % "junit" % "4.12" % "test",
+        "org.apache.commons" % "commons-lang3" % "3.4" % "test"
+      )
+    )
+  )
 
 }
 
